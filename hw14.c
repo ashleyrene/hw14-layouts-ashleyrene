5f5b3757dbc561b82e32dc0d767eae4dcf33f9af
ashleyrene <ashleyrashidian@gmail.com>
2025-04-29 10:35:04 -0400

    Create hw14.c

---
+++ hw14.c
@@ -0,0 +1,9 @@
+#include "hw14.h"
+
+void layout(struct DOMNode *root, float width, float height, FILE *target) {
+  // TODO: fill this in
+}
+
+void free_DOMTree(struct DOMNode *root) {
+  // TODO: fill this in
+}
